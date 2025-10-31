4df661c252b5b090a0a697b91ca57bef12a20ef0
Anslie Pettit <apettit1112@gmail.com>
2025-10-31 13:49:03 -0700

    step7: Fetch GIFs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 console.log("script.js loaded");
 // Request URL
-https://api.giphy.com/v1/gifs/search?api_key=Sa1PMDgwgUnxP5Fl1QcHIJ7FoEJqzOxG&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=Sa1PMDgwgUnxP5Fl1QcHIJ7FoEJqzOxG&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+fetch(endpoint)
+  .then(response => response.json())
+  .then(data => {
+    const images = data.data.map(gif => gif.images.original.url);
+    console.log(images);
+  });
